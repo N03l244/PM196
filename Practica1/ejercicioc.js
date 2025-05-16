b1577324552562f463398eb7eafbd6576f36c968
Noel Betanzos De La Cruz <122043746@upq.edu.mx>
2025-05-16 17:50:18 -0600

    Create ejercicioc.js

---
+++ Practica1/ejercicioc.js
@@ -0,0 +1,8 @@
+/* Crea una arrow function llamada saludoPesonalizado que reciba dos parametros:
+ nombre y edad, y retorne una cadena como la siguiente
+"Hola, me llamo Noel y tengo 21 años"
+*/ 
+
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+console.log(saludoPersonalizado("Isay", 37)); 
