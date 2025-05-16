1712b22a7e21865751e91ee024c8cfcc46bfb44c
Noel Betanzos De La Cruz <122043746@upq.edu.mx>
2025-05-16 17:12:23 -0600

    Ejercicio B
    /b. Convierte esta función tradicional a una arrow function que haga exactamente lo mismo

---
+++ Practica1/ejerciciob.js
@@ -0,0 +1,9 @@
+//b. Convierte esta función tradicional a una arrow function que haga exactamente lo mismo:
+
+/*function cuadrado(nummero) {
+    return nummero * nummero;
+}*/
+
+const cuadrado = (nummero) => nummero * nummero;
+
+console.log(cuadrado(8)); 
