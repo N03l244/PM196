ea38bec05004b573cb342756a2f3ba5b86b7b202
Noel Betanzos De La Cruz <122043746@upq.edu.mx>
2025-06-27 14:30:14 -0600

    acance

--- myappBlank/index.js
+++ myappBlank/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './imgBgrd';
+import App from './ActivityIndicator';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
