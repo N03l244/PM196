d715a9c9177d3e299fd6d9cccf6f7074c3a1434c
Noel Betanzos De La Cruz <122043746@upq.edu.mx>
2025-06-30 18:46:15 -0600

    Scroll

--- myappBlank/index.js
+++ myappBlank/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './Pract11';
+import App from './scroll';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
